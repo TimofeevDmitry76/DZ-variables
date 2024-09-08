f3fb2e5f4c1106bd67cdc7d3c129cb5edc28e5fd
TimofeevDmitry76 <Timofey-76@mail.ru>
2024-09-08 19:18:57 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+completed_tasks = 12
+hours_spend = '1.5'
+name_of_cursis = 'Python,'
+time_for_task = '0.125'
+print(1.5/12)
+print('Курс:',name_of_cursis,'Всего задач:',completed_tasks,',','затрачено часов:',hours_spend,',','среднее время выполнения',time_for_task)
