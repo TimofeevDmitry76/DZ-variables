2987be7733836c15585a02a018dafa8c17d02d6f
TimofeevDmitry76 <Timofey-76@mail.ru>
2024-09-09 17:26:05 +0300

    Устранены замечания.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 completed_tasks = 12
-hours_spend = '1.5'
-name_of_cursis = 'Python,'
-time_for_task = '0.125'
-print(1.5/12)
-print('Курс:',name_of_cursis,'Всего задач:',completed_tasks,',','затрачено часов:',hours_spend,',','среднее время выполнения',time_for_task)
+hours_spend = 1.5
+name_of_course = 'Python,'
+time_for_task = (hours_spend/completed_tasks)
+print('Курс:',name_of_course,'Всего задач:',completed_tasks,',','затрачено часов:',hours_spend,',','среднее время выполнения',time_for_task)
